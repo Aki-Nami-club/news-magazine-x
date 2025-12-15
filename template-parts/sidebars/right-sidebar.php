f180325a227e9af97ce7837fcc228c69b3ac64f0
Aki-Nami-club <dima.radioshop@gmail.com>
2025-12-15 10:55:29 +0200

    Add files via upload

--- template-parts/sidebars/right-sidebar.php
+++ template-parts/sidebars/right-sidebar.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-$current_page = esc_attr($args['page']);
+$current_page = $args['page'];
 
 ?>
 
 <div class="newsx-sidebar widget-area secondary <?php echo 'newsx-'. esc_attr($current_page) .'-sidebar'; ?>">
-    <?php newsx_dynamic_sidebar( $current_page .'-left-sidebar' ); ?>
+    <?php newsx_dynamic_sidebar( $current_page .'-right-sidebar' ); ?>
 </div>
